21b38760e59bbbca3807c1300811bb3f777d7864
Ned Batchelder <ned@nedbatchelder.com>
2017-11-28 06:49:15 -0500

    Oops pnarrow

--- djstell/bin/makehtml.py
+++ djstell/bin/makehtml.py
@@ -180,7 +180,6 @@ class CmdLine(object):
         """Total hack expedient to only process some blog posts."""
         # Change this pattern if you want to fiddle with another one.
         loadpages.blog_pattern = "*thing*.bx"
-        loadpages.page_pattern = "*x*.px"
 
     def do_pnarrow(self):
         """Total hack expedient to only process some pages."""
