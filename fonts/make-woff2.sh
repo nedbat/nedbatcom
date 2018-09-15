de6b1db75b5e1a894b51ea02e01ee1e58d6718be
Ned Batchelder <ned@nedbatchelder.com>
2018-09-15 11:46:46 -0400

    woff2-maker and the woff2 files

---
+++ fonts/make-woff2.sh
@@ -0,0 +1,4 @@
+ttx --flavor woff2 -f -o ../files/PTSerif-NB-Bold.woff2 PTSerif-Bold.ttx
+ttx --flavor woff2 -f -o ../files/PTSerif-NB-BoldItalic.woff2 PTSerif-BoldItalic.ttx
+ttx --flavor woff2 -f -o ../files/PTSerif-NB-Italic.woff2 PTSerif-Italic.ttx
+ttx --flavor woff2 -f -o ../files/PTSerif-NB-Regular.woff2 PTSerif-Regular.ttx
