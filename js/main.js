bce9d2fee7231defb4d9f78b4c55f98153743af5
Ned Batchelder <ned@nedbatchelder.com>
2019-07-21 13:26:43 -0400

    Get rid of fragment highlighting

--- js/main.js
+++ js/main.js
@@ -9,3 +9,26 @@ function nospam(user,domain,args) {
 	}
 	window.location = loc;
 }
+
+// Manipulate the submit button on the comment form.
+jQuery(function($){
+    // Make the add button wait 2 secs to really submit.
+    $("#addbtn").click(function(){
+        $.timer(2000, function(timer){
+            $("#addbtn").unbind().click();
+            timer.stop();
+        });
+        return false;
+    });
+    // In 2 seconds,
+    $.timer(2000, function(timer) {
+        // remove our custom click function.
+        $("#addbtn").unbind();
+        timer.stop();
+    });
+});
+
+jQuery(function($){
+    var thisdate = new Date().toJSON().slice(5,10).replace('-', '');
+    $(".thisdate").attr("href", "/blog/archive/date" + thisdate + ".html");
+});
