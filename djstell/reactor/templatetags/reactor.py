77d2d3d7e1a5e5091214408ea2ec1dd7bf5e4175
Ned Batchelder <ned@nedbatchelder.com>
2021-08-23 07:51:48 -0400

    comment labels

--- djstell/reactor/templatetags/reactor.py
+++ djstell/reactor/templatetags/reactor.py
@@ -5,6 +5,18 @@ from djstell.reactor.models import Comment
 
 register = template.Library()
 
+
+@register.simple_tag
+def comment_label(entryid):
+    num = Comment.objects.filter(entryid=entryid).count()
+    if num == 0:
+        return "react"
+    elif num == 1:
+        return "1 reaction"
+    else:
+        return f"{num} reactions"
+
+
 @register.inclusion_tag("comments.html")
 def entry_comments(entryid):
     comments = Comment.objects.filter(entryid=entryid).order_by("posted")
