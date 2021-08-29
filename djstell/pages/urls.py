089bed487d396e0493ddb4646a95008c88860293
Ned Batchelder <ned@nedbatchelder.com>
2021-08-29 08:05:50 -0400

    convert urls to path and re_path instead of url()
    RemovedInDjango40Warning: django.conf.urls.url() is deprecated in favor of django.urls.re_path().

--- djstell/pages/urls.py
+++ djstell/pages/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path, re_path, register_converter
 from django.views.generic.base import TemplateView
 
 import djstell.pages.views as dpv
@@ -14,28 +14,44 @@ PLANET_PYTHON_TAGS = [
     'jupyter', 'opensource', 'presentations', 'linters', 'regex',
     ]
 
+def digits(n):
+    class DigitConverter:
+        regex = f'[0-9]{{{n}}}'
+
+        def to_python(self, value):
+            return int(value)
+
+        def to_url(self, value):
+            return f'%0{n}d' % value
+    return DigitConverter
+
+register_converter(digits(4), 'yyyy')
+register_converter(digits(2), 'mm')
+register_converter(digits(2), 'dd')
+
+
 urlpatterns = [
-    url(r'^(?:index.html)?$', dpv.index),
-    url(r'^blog(?:/index.html)?$', dpv.blogmain),
-    url(r'^blog/(?P<year>\d\d\d\d)(?P<month>\d\d)/(?P<slug>[^/]+).html$', dpv.entry),
+    re_path(r'^(?:index.html)?$', dpv.index),
+    re_path(r'^blog(?:/index.html)?$', dpv.blogmain),
+    path('blog/<yyyy:year><mm:month>/<slug:slug>.html', dpv.entry),
 
-    url(r'^blog/tags.html$', dpv.tags),
-    url(r'^blog/tag/none.html$', dpv.untagged),
-    url(r'^blog/tag/(?P<slug>.*).html$', dpv.tag),
+    path('blog/tags.html', dpv.tags),
+    path('blog/tag/none.html', dpv.untagged),
+    path('blog/tag/<slug:slug>.html', dpv.tag),
 
-    url(r'^blog/archive/year(?P<year>\d\d\d\d).html$', dpv.archiveyear),
-    url(r'^blog/archive/date(?P<month>\d\d)(?P<day>\d\d).html$', dpv.archivedate),
-    url(r'^blog/archive/all.html$', dpv.archiveall),
-    url(r'^blog/drafts.html$', dpv.drafts),
+    path('blog/archive/year<yyyy:year>.html', dpv.archiveyear),
+    path('blog/archive/date<mm:month><dd:day>.html', dpv.archivedate),
+    path('blog/archive/all.html', dpv.archiveall),
+    path('blog/drafts.html', dpv.drafts),
 
-    url(r'^blog/rss.xml$', dpv.blog_rss),
-    url(r'^blog/planetpython.xml$', dpv.tags_rss, {'tags': PLANET_PYTHON_TAGS}),
-    url(r'^blog/moved.php$', dpv.blog_moved_php),
+    path('blog/rss.xml', dpv.blog_rss),
+    path('blog/planetpython.xml', dpv.tags_rss, {'tags': PLANET_PYTHON_TAGS}),
+    path('blog/moved.php', dpv.blog_moved_php),
 
-    url(r'^(?P<path>(text|code|site)/?.*)$', dpv.article),
-    url(r'^(?P<path>err404.html)$', dpv.article),
+    re_path(r'^(?P<path>(text|code|site)/?.*)$', dpv.article),
+    re_path(r'^(?P<path>err404.html)$', dpv.article),
 
-    url(r'^0inc/sidebar_(?P<which>\w+).inc$', dpv.sidebar),
-    url(r'^0inc/navbar.inc$', dpv.navbar),
-    url(r'^0inc/metatags.inc$', dpv.metatags),
+    path('0inc/sidebar_<slug:which>.inc', dpv.sidebar),
+    path('0inc/navbar.inc', dpv.navbar),
+    path('0inc/metatags.inc', dpv.metatags),
     ]
