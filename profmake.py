391fbcaa76c636f2325fd50f9b6ad11f4fe92789
Ned Batchelder <ned@nedbatchelder.com>
2021-09-22 09:56:23 -0400

    obsolete: we don't need make or xuff to be fast

--- profmake.py
+++
@@ -1,8 +0,0 @@
-#import stellated.XuffApp
-from djstell.bin.makehtml import CmdLine
-import profile, pstats
-import sys
-
-profile.run('CmdLine().main(sys.argv[1:])', 'profmake')
-p = pstats.Stats('profmake')
-p.strip_dirs().sort_stats('cumulative').print_stats(30)
--- profxuff.py
+++
@@ -1,7 +0,0 @@
-import stellated.XuffApp
-import profile, pstats
-import sys
-
-profile.run('stellated.XuffApp.XuffApp().main(sys.argv)', 'xuffprof')
-p = pstats.Stats('xuffprof')
-p.strip_dirs().sort_stats('cumulative').print_stats(30)
