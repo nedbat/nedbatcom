e443f319b141624d10f1f50ff5f24c5be98804cc
Ned Batchelder <ned@nedbatchelder.com>
2021-09-26 18:24:00 -0400

    no need for printing exceptions to stdout on real servers

--- djstell/middleware/standard.py
+++ djstell/middleware/standard.py
@@ -12,8 +12,10 @@ class AnnounceErrorsMiddleware(MiddlewareMixin):
     """ Make sure exceptions get mentioned on stdout, for use while generating
         static pages.
     """
-    def process_exception(self, request, exception):
-        print(ascii("Error: %s -> %s" % (request.path, exception)))
+    if settings.LOCAL_LIVE:
+        def process_exception(self, request, exception):
+            print(ascii("Error: %s -> %s" % (request.path, exception)))
+
 
 class StaticOverlayMiddleware(MiddlewareMixin):
     """ Serve static files if they exist, otherwise let Django handle it.
