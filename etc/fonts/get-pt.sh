a4f0c6a13d936e3225bb101e71ab7a5d539f8fd6
Ned Batchelder <ned@nedbatchelder.com>
2023-05-30 05:37:22 -0400

    fonts: no need for custom PT-Serif anymore

--- etc/fonts/get-pt.sh
+++
@@ -1,9 +0,0 @@
-# URLs from https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i|Source+Sans+Pro:400,400i,700,700i
-wget -O PTSerif-Italic.ttf https://fonts.gstatic.com/s/ptserif/v9/EJRTQgYoZZY2vCFuvAFT_r21dw.ttf
-wget -O PTSerif-BoldItalic.ttf https://fonts.gstatic.com/s/ptserif/v9/EJRQQgYoZZY2vCFuvAFT9gaQZynfpQ.ttf
-wget -O PTSerif-Regular.ttf https://fonts.gstatic.com/s/ptserif/v9/EJRVQgYoZZY2vCFuvAFWzro.ttf
-wget -O PTSerif-Bold.ttf https://fonts.gstatic.com/s/ptserif/v9/EJRSQgYoZZY2vCFuvAnt66qSVy4.ttf
-ttx PTSerif-Italic.ttf
-ttx PTSerif-BoldItalic.ttf 
-ttx PTSerif-Regular.ttf 
-ttx PTSerif-Bold.ttf 
