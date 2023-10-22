f655e4186339cb7040a1267754f20f0d473721ed
Ned Batchelder <ned@nedbatchelder.com>
2023-10-22 14:47:06 -0400

    don't use /stable/ any more

--- djstell/pages/urls.py
+++ djstell/pages/urls.py
@@ -50,9 +50,9 @@ urlpatterns = [
     path('summary.json', dpv.summary),
     re_path(r'^blog/(?P<whenid>\d{8}T\d{6}).html$', dpv.entry_by_date),
 
-    re_path(r'^code/coverage/?$', redirect(url='https://coverage.readthedocs.org/en/stable/')),
-    path('code/coverage/beta/<path:path>', redirect(url='https://coverage.readthedocs.org/en/stable/%(path)s')),
-    path('code/coverage/<path:path>', redirect(url='https://coverage.readthedocs.org/en/stable/%(path)s')),
+    re_path(r'^code/coverage/?$', redirect(url='https://coverage.readthedocs.org/en/latest/')),
+    path('code/coverage/beta/<path:path>', redirect(url='https://coverage.readthedocs.org/en/latest/%(path)s')),
+    path('code/coverage/<path:path>', redirect(url='https://coverage.readthedocs.org/en/latest/%(path)s')),
     path('code/modules/coverage.html', redirect(url='https://coverage.readthedocs.org')),
     path('code/modules/coverage-<path:path>', redirect(url='https://pypi.org/project/coverage/#files')),
     re_path(r'^(?P<path>(text|code|site))$', dpv.article),
