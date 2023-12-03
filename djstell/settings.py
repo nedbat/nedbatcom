7ba4262de1bf4bd0dc80b47e976bac13e5c010ba
Ned Batchelder <ned@nedbatchelder.com>
2023-12-03 09:38:01 -0500

    set USE_TZ to be ready for Django 5
    Running the tests gave me a warning:
    
    RemovedInDjango50Warning: The default value of USE_TZ will change from False to True in Django 5.0. Set USE_TZ to False in your project settings if you want to keep the current default behavior.

--- djstell/settings.py
+++ djstell/settings.py
@@ -47,6 +47,7 @@ DATABASE_ROUTERS = ['djstell.reactor.models.ReactorRouter']
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'America/New_York'
+USE_TZ = False
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
