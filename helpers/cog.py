c26e58a839d4037c80bfbb06eda9703b9d8687a9
Ned Batchelder <ned@nedbatchelder.com>
2026-03-04 06:14:40 -0500

    remove a cog helper we no longer need

--- helpers/cog.py
+++ helpers/cog.py
@@ -7,30 +7,6 @@ from pathlib import Path
 from edtext import EdText
 
 
-def include_section(filename, start, end, prelude="", postlude=""):
-    """
-    Grab a part of a file, and print it for inclusion with cog.
-
-    Args:
-        start (str): The text marking the start of the section. This line is not
-            included in the output.
-        end (str): The text marking the end of the section. This line is not
-            included in the output.
-        prelude (str): Line(s) to be output before the section.
-        postlude (str): Line(s) to be output after the section.
-
-    """
-    with open(filename) as f:
-        lines = list(f)
-    start_num = next(line_num for line_num, line in enumerate(lines) if line.strip() == start)
-    end_num = next(line_num for line_num, line in enumerate(lines[start_num:], start=start_num) if line.strip() == end)
-    if prelude:
-        print(prelude)
-    print("".join(lines[start_num+1: end_num]), end="")
-    if postlude:
-        print(postlude)
-
-
 def ed(filename):
     return EdText(Path(filename).read_text())
 
